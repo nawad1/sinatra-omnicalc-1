9de54a0b4651a6a88a7a2c3e348f93a667ec8e2c
nawad1 <nawad1@chicagobooth.edu>
2024-07-05 14:10:46 +0000

    base template

--- app.rb
+++ app.rb
@@ -2,8 +2,39 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:new)
+end
+
+get("/square/new") do
+  erb(:square)
+end
+
+get("/square/results") do
+  @the_num = params.fetch("number").to_f
+  @the_result = @the_num ** 2
+  erb(:square_results)
+end
+
+get("/square_root/new") do
+  erb(:square_root)
+end
+
+get("/square_root/results") do
+  erb(:square_root_results)
+end
+
+get("/payment/new") do
+  erb(:payment)
+end
+
+get("/payment/results") do
+  erb(:payment_results)
+end
+
+get("/random/new") do
+  erb(:random)
+end
+  
+get("/random/results") do
+  erb(:random_results)
 end
