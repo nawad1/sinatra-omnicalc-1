29eac64c7e5d0303f11eb3d184fd6fc07b326a79
nawad1 <nawad1@chicagobooth.edu>
2024-07-05 16:06:54 +0000

    final app

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  erb(:new)
+  erb(:square)
 end
 
 get("/square/new") do
@@ -20,6 +20,8 @@ get("/square_root/new") do
 end
 
 get("/square_root/results") do
+  @the_num = params.fetch("user_number").to_f
+  @the_result = Math.sqrt(@the_num)
   erb(:square_root_results)
 end
 
@@ -28,6 +30,10 @@ get("/payment/new") do
 end
 
 get("/payment/results") do
+  @rate = params.fetch("user_apr").to_f / 100 / 12
+ @number_periods = params.fetch("user_years").to_f * 12
+  @present_value = params.fetch("user_pv").to_f
+ @payment = (@rate * @present_value) / (1-(1+@rate)**(@number_periods * -1))
   erb(:payment_results)
 end
 
@@ -36,5 +42,8 @@ get("/random/new") do
 end
   
 get("/random/results") do
+  @min_num = params.fetch("user_min").to_f
+  @max_num = params.fetch("user_max").to_f
+  @rand_num = rand(@min_num...@max_num)
   erb(:random_results)
 end
